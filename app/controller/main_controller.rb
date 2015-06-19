7bf25b80a41301f079bace6e4ea635ed4facb68a
codeamac02 <codeamac02@gmail.com>
2015-06-18 20:32:55 -0500

    Add option create to ARGV with respective views. Check if user enter enough ARGV attributes.

--- app/controller/main_controller.rb
+++ app/controller/main_controller.rb
@@ -6,33 +6,64 @@ class MainController
 		# @view.interface
 	end
 	
+	def check_enough_argv_attributes
+		if @argv.length < 2
+			@view.error_argv_elements 
+			true
+		else
+			false
+		end
+	end
+
 	def command_case(argv)
 		@argv = argv
-		command = @argv[0]
-		# puts "command: #{command}"
-		# input_class = @argv[1]
-		# input_class.capitalize
-		# puts "input_class: #{input_class}"
-		# p @argv
 
-		case command
-		when "index"
-			# puts "Inside index"
-			@view.index(Chef.all)
-		when "create"
-			# puts "Inside create"
-			@view.create_chef(Chef.create(name: "Chefcito", email: "chefcito@mail.com"))
-		when "update"
-			# puts "Inside update"
-			chef = Chef.find_by(name: "Chefcito")
-			name_before = chef.name
-			chef.name = "Dany"
-			name_after = chef.name
-			chef.save
-			@view.update_chef(name_before, name_after)
-		when "delete"
-			# puts "Inside delete"
-			@view.delete_chef(Chef.destroy(5))
+		if check_enough_argv_attributes
+			return
+		else
+			command = @argv[0]
+			# puts "command: #{command}"
+			argv_1 = @argv[1].dup if @argv[1]
+
+			input_class = argv_1.capitalize!.constantize
+			# puts "input_class: #{input_class}"
+			# p @argv
+
+			case command
+			when "index"
+				# puts "Inside index"
+				@view.index(input_class.all, argv_1)
+			when "create"
+				# puts "Inside create"
+				# @view.create_chef(input_class.create(name: "Chefcito", email: "chefcito@mail.com"))
+
+				if input_class == Chef
+					chef_attributes = @view.get_chef_attributes
+					chef = Chef.create(name: chef_attributes[0], email: chef_attributes[1])
+					@view.create_chef(chef)
+				elsif input_class == Meal
+					meal_attributes = @view.get_meal_attributes
+					meal = Meal.create(chef_id: meal_attributes[0], name: meal_attributes[1], description: meal_attributes[2])
+					@view.create_meal(meal)
+				elsif input_class == Ingredient
+					ingredient_attributes = @view.get_ingredient_attributes
+					ingredient = Ingredient.create(name: ingredient_attributes[0])
+					@view.create_ingredient(ingredient)
+				else
+					@view.option_error
+				end
+			when "update"
+				# puts "Inside update"
+				chef = Chef.find_by(name: "Chefcito")
+				name_before = chef.name
+				chef.name = "Dany"
+				name_after = chef.name
+				chef.save
+				@view.update_chef(name_before, name_after)
+			when "delete"
+				# puts "Inside delete"
+				@view.delete_chef(Chef.destroy(5))
+			end
 		end
 	end
 end
--- app/view/main_view.rb
+++ app/view/main_view.rb
@@ -8,11 +8,23 @@ class MainView
 		puts "-" * 8
 	end
 	
-	def index(list)
-		puts "List of "
+	def index(list, list_class)
+		puts "Index of #{list_class}"
+		puts "-" * (list_class.length + 9)
 		list.each do |instance|
-			puts instance.name
+			puts "#{instance.id}. #{instance.name}"
 		end
+	end
+
+	def get_chef_attributes
+		chef_attributes = []
+		puts "Insert chef name: "
+		chef_name = $stdin.gets.chomp
+		chef_attributes << chef_name
+		puts "Insert chef email: "
+		chef_email = $stdin.gets.chomp
+		chef_attributes << chef_email
+		chef_attributes
 	end
 
 	def create_chef(attributes)
@@ -20,6 +32,44 @@ class MainView
 		puts "-" * 28
 		puts "Name:  #{attributes.name}"
 		puts "Email: #{attributes.email}"
+		puts 
+	end
+
+	def get_meal_attributes
+		meal_attributes = []
+		puts "Insert chef creator id: "
+		chef_id = $stdin.gets.chomp
+		meal_attributes << chef_id
+		puts "Insert meal name: "
+		meal_name = $stdin.gets.chomp
+		meal_attributes << meal_name
+		puts "Insert meal description: "
+		meal_description = $stdin.gets.chomp
+		meal_attributes << meal_description
+		meal_attributes
+	end
+
+	def create_meal(attributes)
+		puts "You have created a new meal:"
+		puts "-" * 28
+		puts "Chef_id:     #{attributes.chef_id}"
+		puts "Name:        #{attributes.name}"
+		puts "Description: #{attributes.description}"
+		puts 
+	end
+
+	def get_ingredient_attributes
+		ingredient_attributes = []
+		puts "Insert ingredient name: "
+		ingredient_name = $stdin.gets.chomp
+		ingredient_attributes << ingredient_name
+		ingredient_attributes
+	end
+
+	def create_ingredient(attributes)
+		puts "You have created a new ingredient:"
+		puts "-" * 28
+		puts "Name:  #{attributes.name}"
 		puts 
 	end
 
@@ -33,5 +83,14 @@ class MainView
 		puts "You have deleted this chef:"
 		puts "-" * 28
 		puts "ID: #{attributes.id}"
+	end
+
+	def error_argv_elements
+		puts "Please add 2 arguments"
+	end
+
+	def option_error
+		puts "Not exist"
+		puts "Use [chef][meal][ingredient]"
 	end
 end
